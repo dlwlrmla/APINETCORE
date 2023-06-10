8ed9088eae7b219acfcc964c580b7a8c0cb3a071
dlwlrmla <joakomask@gmail.com>
2023-06-10 19:36:33 -0400

    Agregar archivos de proyecto.

---
+++ Models/AngularContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProyectoAngular.Models;
+
+public partial class AngularContext : DbContext
+{
+    public AngularContext()
+    {
+    }
+
+    public AngularContext(DbContextOptions<AngularContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Tarea> Tareas { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){ }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Tarea>(entity =>
+        {
+            entity.HasKey(e => e.IdTarea).HasName("PK__tarea__756A5402CE1F2089");
+
+            entity.ToTable("tarea");
+
+            entity.Property(e => e.IdTarea).HasColumnName("idTarea");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(69)
+                .IsUnicode(false)
+                .HasColumnName("nombre");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using ProyectoAngular.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AngularContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("CadenaSQL"));
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("NuevaPolitica", app =>
+    {
+        app.AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseCors("NuevaPolitica");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
